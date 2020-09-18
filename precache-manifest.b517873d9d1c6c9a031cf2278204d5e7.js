383cb44fdce412e0e830a6cf2ee3eb4405eb4691
li2he1 <peterli2he1@gmail.com>
2020-09-18 03:42:56 -0400

    Revert "Updates"
    This reverts commit 410fc455cb95daed0dda6d3742c39b4bf549fb17.

---
+++ precache-manifest.b517873d9d1c6c9a031cf2278204d5e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3870619d669a54d1268e9c84c755ac9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6072dc22e35534529919",
+    "url": "/static/css/main.9dcab7d2.chunk.css"
+  },
+  {
+    "revision": "80fb73300232dc6ef52c",
+    "url": "/static/js/2.05d74e8f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.05d74e8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6072dc22e35534529919",
+    "url": "/static/js/main.a7c293f8.chunk.js"
+  },
+  {
+    "revision": "80aa0bef8f96492eb9b6",
+    "url": "/static/js/runtime-main.cabddb2c.js"
+  }
+]);
